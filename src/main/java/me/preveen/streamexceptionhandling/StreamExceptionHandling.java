cbee2b5b61ebbd953a8c20773879f150cc4cf30e
preveen <preveen@stacklighting.com>
2017-12-03 09:27:38 +0530

    streamexceptionhandling : javaslang try monad

--- src/main/java/me/preveen/streamexceptionhandling/StreamExceptionHandling.java
+++ src/main/java/me/preveen/streamexceptionhandling/StreamExceptionHandling.java
@@ -9,6 +9,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.Optional;
 import java.util.stream.Stream;
+import javaslang.control.*;
 
 /**
  * @author linkbee
@@ -21,7 +22,7 @@ public class StreamExceptionHandling {
 	 * Converts given date string in format "MM/dd/yyyy" to LocalDate.
 	 * Returns Optional of LocalDate if it is valid, otherwise Optional.empty
 	 */
-	private static Optional<LocalDate> parseDate(String dateString){
+	private static Optional<LocalDate> parseDateOptional(String dateString){
 	    LocalDate localDate = null;
 	    try {
 	        localDate = LocalDate.from(formatter.parse(dateString));
@@ -29,6 +30,14 @@ public class StreamExceptionHandling {
 	        System.out.println(e.getMessage());
 	    }
 	    return Optional.ofNullable(localDate);
+	}
+	
+	/**
+	 * Converts given date string in format "MM/dd/yyyy" to LocalDate.
+	 * Returns Try of LocalDate representing valid LocalDate or Throwable in case of invalid value.
+	 */
+	private static Try<LocalDate> parseDateTryMonad(String dateString){
+	    return Try.of(() -> LocalDate.from(formatter.parse(dateString)));
 	}
 	/**
 	 * @param args
@@ -40,10 +49,22 @@ public class StreamExceptionHandling {
 	            "12/31/2015",
 	            "not a date",
 	            "01/01/2016")
-	            .map(StreamExceptionHandling::parseDate)//Parse String to LocalDate
+	            .map(StreamExceptionHandling::parseDateOptional)//Parse String to LocalDate
 	            .filter(Optional::isPresent) //Filter valid ones
 	            .map(Optional::get)//Get wrapped LocalDate
 	            .map(DayOfWeek::from) //Map to day of week
 	            .forEach(System.out::println); //Print
+		
+		Stream.of("12/31/2014",
+	            "01-01-2015",
+	            "12/31/2015",
+	            "not a date",
+	            "01/01/2016")
+	            .map(StreamExceptionHandling::parseDateTryMonad)//Parse String to LocalDate
+	            .peek(v-> v.onFailure(t -> System.out.println("Failed due to " + t.getMessage())))//Print error on failure
+	            .filter(Try::isSuccess)//Filter valids
+	            .map(Try::get)//Get wrapped value
+	            .map(DayOfWeek::from)//Map to day of week
+	            .forEach(System.out::println);//Print
     }
 }
